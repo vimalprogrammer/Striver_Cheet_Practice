f34be084fcae0ba705ad26635512342285d80a85
Vimal M <msvimal10@gmail.com>
2021-08-06 20:55:19 +0530

    Find the duplicate in an array(inclusive) of N+1 integers using linkedlist cycle method

---
+++ duplicate.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution{
+public:
+    int dup(vector<int>& nums)
+    {
+        int slow=nums[0];
+        int fast=nums[0];
+        do
+        {
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+        }while(slow!=fast);
+        fast=nums[0];
+        while(slow!=fast)
+        {
+            slow=nums[slow];
+            fast=nums[fast];
+        }
+        return slow;
+    }
+};
+int main()
+{
+    vector<int> nums={2,5,9,6,9,3,8,9,7,1};
+    int res;
+    Solution s;
+    res=s.dup(nums);
+    cout<<res;
+    return 0;
+}
