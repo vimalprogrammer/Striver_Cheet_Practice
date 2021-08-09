5ad76046c62a77370e085dcab5f55283a058a307
Vimal M <msvimal10@gmail.com>
2021-08-09 23:55:38 +0530

    O(n)Maximum Sub Array Sum-Kadane's algo

--- MaxSubArrSum.cpp
+++ MaxSubArrSum.cpp
@@ -26,3 +26,39 @@ int main()
     }
     cout<<max_;
 }
+
+//Optimal approach O(n) ----> Kadane's algorithm
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxi(int a,int b)
+{
+    if(a>b)
+        return a;
+    return b;
+}
+class Solution
+{
+    public:
+    int MaxSubArrSum(vector<int>& nums)
+    {
+        int sum=0,max_=nums[0];
+        for(auto it:nums)
+        {
+            sum+=it;
+            max_=maxi(sum,max_);
+            if(sum<0)
+                sum=0;
+        }
+        return max_;
+    }
+};
+
+int main()
+{
+    vector<int> nums={-2,-3,4,-1,-2,1,5,-3};
+    Solution s;
+    cout<<s.MaxSubArrSum(nums);
+    return 0;
+}
