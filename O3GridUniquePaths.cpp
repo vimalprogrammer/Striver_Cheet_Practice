7623f65785d71884b17eae65f59b25d29ca0e942
Vimal M <msvimal10@gmail.com>
2021-08-29 13:15:10 +0530

    start to reach end in grid google ques

---
+++ O3GridUniquePaths.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    
+    int sol(int m,int n,int i,int j,vector<vector<int>>&dp)
+    {
+        if((i==(m-1)) && (j==(n-1)))
+            return 1;
+        if((i>=m) || (j>=n))
+            return 0;
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        else
+            return dp[i][j]=(sol(m,n,i+1,j,dp)+sol(m,n,i,j+1,dp));
+    }
+    
+    int uniquePaths(int m, int n) {
+        
+        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+        int i=0,j=0;
+        return sol(m,n,i,j,dp);
+    }
+};
