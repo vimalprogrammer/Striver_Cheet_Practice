b227d72dddd108c45934f4c3b79a43eee3cba319
Vimal M <msvimal10@gmail.com>
2021-08-31 01:06:18 +0530

    Comments added

--- O3ReversePairs.cpp
+++ O3ReversePairs.cpp
@@ -1,6 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+/*
+ Given an integer array nums, return the number of reverse pairs in the array.
+A reverse pair is a pair (i, j) where 0 <= i < j < nums.length and nums[i] > 2
+ * nums[j]
+
+Input: nums = [40,25,19,12,9,6,2]
+Output: 15
+Explanation: There are 15 reverse pairs in the given array.
+40>2*19 40>2*12 40>2*9 40>2*6 40>2*2
+25>2*12 25>2*9 25>2*6  25>2*2
+19>2*9 19>2*6  19>2*2 
+12>2*9 
+9>2*6 
+2>2*6
+*/
+
 class Solution {
     
     int merge(vector<int>&nums,int low,int mid,int high)
