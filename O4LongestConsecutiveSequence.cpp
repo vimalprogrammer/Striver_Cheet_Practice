707dc65c994b379e1d0c9a5ba1e6962553806826
Vimal M <msvimal10@gmail.com>
2021-09-03 21:17:57 +0530

    Test cases added

---
+++ O4LongestConsecutiveSequence.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+Input: 102,4,100,1,101,3,2
+Output: 4
+Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+*/
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {        
+        unordered_set<int>hashset;
+        for(int it:nums)
+            hashset.insert(it);
+        int longestStreak=0;
+        for(int i:nums)
+        {
+            if(!hashset.count(i-1))
+               {
+                   int curStreak=1;
+                   int curNum=i;
+                   
+                   while(hashset.count(curNum+1))
+                   {
+                       curStreak++;
+                       curNum++;
+                   }
+                   longestStreak=max(curStreak,longestStreak);
+               }
+        }
+        return longestStreak;
+    }
+};
