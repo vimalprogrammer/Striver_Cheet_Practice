2f649aa23ee1dd4472fcc671e3aa3bb356dc5ce3
Vimal M <msvimal10@gmail.com>
2021-09-08 21:07:07 +0530

    Count SubArr With Xor = K

---
+++ O5CountSubArrWithXorK.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+Input: arr[] = {4, 2, 2, 6, 4}
+       k = 6
+Output: cnt=4
+Explanation: There are 4 subarrays with sum 6.
+*/   
+    
+
+int Solution::solve(vector<int> &A, int B) {
+    map<int,int>mp;
+    int cnt=0,xorr=0;
+    for(auto it:A)
+    {
+        xorr=xorr^it;
+        if(xorr==B)
+            cnt++;
+        if(mp.find(xorr^B)!=mp.end())
+            cnt+=mp[xorr^B];
+        
+        mp[xorr]++;
+    }
+    return cnt;
+}
