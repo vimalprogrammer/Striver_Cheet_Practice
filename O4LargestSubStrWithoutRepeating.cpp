7ddb00db7fb301cd3293638d976c04bb6983b738
Vimal M <msvimal10@gmail.com>
2021-09-10 00:00:44 +0530

    Striver solution with test cases added

--- O4LargestSubStrWithoutRepeating.cpp
+++ O4LargestSubStrWithoutRepeating.cpp
@@ -1,6 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+/*
+Given a string, find the length of the longest substring without repeating characters.
+Input: "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+Input: "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+Input: "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+*/
+
 /*class Solution {
 public: 
     int lengthOfLongestSubstring(string s) {
@@ -28,3 +42,25 @@ public:
      }
 };
 */
+
+class Solution {
+public: 
+    int lengthOfLongestSubstring(string s) {
+        
+        int len=0;
+        int left=0,right=0;
+        int n=s.size();
+        vector<int>mpp(256,-1);
+        
+        while(right<n)
+        {
+            if(mpp[s[right]]!=-1)
+                left=max(mpp[s[right]]+1,left);
+            
+            mpp[s[right]]=right;
+            len=max(len,right-left+1);
+            right++;
+        }
+        return len;
+     }
+};
